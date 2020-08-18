a87601780e2c1b626d1631e90e569bab6146cf38
viniciusll <viniciuslemos179@gmail.com>
2020-08-18 16:28:08 -0300

    terminar em casa

--- src/views/Candidatos/sections/formulario1.jsx
+++ src/views/Candidatos/sections/formulario1.jsx
@@ -1,165 +1,231 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InputGroup, Input, Button, Label, FormGroup, Form, CustomInput } from 'reactstrap';
+import api from '../../../api/ConnectApi';
 
 const Formulario1 = () => {
+    const [nomeCompletoCandidato, setNomeCompletoCandidato] = useState('');
+    const [nomeComoCandidato, setNomeComoCandidato] = useState('');
+    const [candidatoA, setCandidatoA] = useState('');
+    const [cidadeEntrega, setCidadeEntrega] = useState('');
+    const [enderecoEntrega, setEnderecoEntrega] = useState('');
+    const [estadoEntrega, setEstadoEntrega] = useState('');
+    const [cepEntrega, setCepEntrega] = useState('');
+    const [enderecoNotaFiscal, setEnderecoNotaFiscal] = useState('');
+    const [cidadeNotaFiscal, setCidadeNotaFiscal] = useState('');
+    const [estadoNotaFiscal, setEstadoNotaFiscal] = useState('');
+    const [cepNotaFiscal, setCepNotaFiscal] = useState('');
+    const [inscRg, setInscRg] = useState('');
+    const [cnpjOuCpf, setCnpjOuCpf] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [email, setEmail] = useState('');
+    const [exemplar, setExemplar] = useState('');
+    const [nomeDoRepresentanteComercial, setNomeDoRepresentanteComercial] = useState('');
+    const [cidadeDoRepresentanteComercial, setCidadeDoRepresentanteComercial] = useState('');
+    const [files, setFiles] = useState([]);
+
+    const handleUpload = files => {
+        console.log(files);
+        setFiles(files);
+    };
+
+    const enviarFormulario = async () => {
+        const data = new FormData();
+        data.append('files', files);
+        data.append('nomeCompletoCandidato', nomeCompletoCandidato);
+        data.append('nomeComoCandidato', nomeComoCandidato);
+        data.append('candidatoA', candidatoA);
+        data.append('cidadeEntrega', cidadeEntrega);
+        data.append('enderecoEntrega', enderecoEntrega);
+        data.append('estadoEntrega', estadoEntrega);
+        data.append('cepEntrega', cepEntrega);
+        data.append('enderecoNotaFiscal', enderecoNotaFiscal);
+        data.append('cidadeNotaFiscal', cidadeNotaFiscal);
+        data.append('estadoNotaFiscal', estadoNotaFiscal);
+        data.append('cepNotaFiscal', cepNotaFiscal);
+        data.append('inscRg', inscRg);
+        data.append('cnpjOuCpf', cnpjOuCpf);
+        data.append('telefone', telefone);
+        data.append('email', email);
+        data.append('exemplar', exemplar);
+        data.append('nomeDoRepresentanteComercial', nomeDoRepresentanteComercial);
+        data.append('cidadeDoRepresentanteComercial', cidadeDoRepresentanteComercial);
+
+        const request = await api.post('/ficha/criarFormulario1', data);
+        console.log(request);
+    }
+
     return (
         <div>
-            <Form style={{ padding: '10px', alignItems: 'center'}}>
+            <Form className="uploader" encType="multipart/form-data" style={{ padding: '10px', alignItems: 'center'}}>
                 <FormGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="Nome completo do Candidato:" />
+                        <Input onChange={e => setNomeCompletoCandidato(e.target.value)} placeholder="Nome completo do Candidato:" />
                     </InputGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="Nome como candidato:" />
+                        <Input onChange={e => setNomeComoCandidato(e.target.value)} placeholder="Nome como candidato:" />
                     </InputGroup>
                 </FormGroup>
                 <div style={{ textAlign: 'center'}}>
                 <p>Candidato a: </p>
                     <FormGroup style={{ paddingTop: '10px' }} check inline>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Prefeito' onChange={e => setCandidatoA(e.target.value)} type="radio" name='radio1' />{' '}
                             Prefeito
                         </Label>
                         <Label check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Vereador' onChange={e => setCandidatoA(e.target.value)} type="radio" name='radio1' />{' '}
                             Vereador
                         </Label>
                     </FormGroup>
                 </div>
                 <FormGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="Endereço para entrega:" />
+                        <Input onChange={e => setEnderecoEntrega(e.target.value)} placeholder="Endereço para entrega:" />
                     </InputGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="Cidade para entrega:" />
+                        <Input onChange={e => setCidadeEntrega(e.target.value)} placeholder="Cidade para entrega:" />
                     </InputGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="Estado para entrega:" />
+                        <Input onChange={e => setEstadoEntrega(e.target.value)} placeholder="Estado para entrega:" />
                     </InputGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="CEP para entrega:" />
+                        <Input onChange={e => setCepEntrega(e.target.value)} placeholder="CEP para entrega:" />
                     </InputGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="Endereço para nota fiscal:" />
+                        <Input onChange={e => setEnderecoNotaFiscal(e.target.value)} placeholder="Endereço para nota fiscal:" />
                     </InputGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="Cidade para nota fiscal:" />
+                        <Input onChange={e => setCidadeNotaFiscal(e.target.value)} placeholder="Cidade para nota fiscal:" />
                     </InputGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="Estado para nota fiscal:" />
+                        <Input onChange={e => setEstadoNotaFiscal(e.target.value)} placeholder="Estado para nota fiscal:" />
                     </InputGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="CEP para nota fiscal:" />
+                        <Input onChange={e => setCepNotaFiscal(e.target.value)} placeholder="CEP para nota fiscal:" />
                     </InputGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="Insc. Est./RG:" />
+                        <Input onChange={e => setInscRg(e.target.value)} placeholder="Insc. Est./RG:" />
                     </InputGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="CNPJ/CPF:" />
+                        <Input onChange={e => setCnpjOuCpf(e.target.value)} placeholder="CNPJ/CPF:" />
                     </InputGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="Telefone:" />
+                        <Input onChange={e => setTelefone(e.target.value)} placeholder="Telefone:" />
                     </InputGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="E-mail:" />
+                        <Input onChange={e => setEmail(e.target.value)} placeholder="E-mail:" />
                     </InputGroup>
                 </FormGroup>
                 <div style={{ textAlign: 'center'}}>
                 <p>Quantidade de Exemplares: </p>
                     <FormGroup style={{ paddingTop: '10px' }} check row>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Edição com 8 páginas + 4 capas - Quantidade: 250 Exemplares - Valor: R$877,50'
+                                type="radio" onChange={e => setExemplar(e.target.value)} name='radio1' />{' '}
                             Edição com 8 páginas + 4 capas – Quantidade: 250 Exemplares - Valor: R$877,50
                         </Label>
                     </FormGroup>
                     <FormGroup style={{ paddingTop: '10px' }} check row>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Edição com 8 páginas + 4 capas – Quantidade: 500 Exemplares - Valor: R$1.485,00' 
+                                onChange={e => setExemplar(e.target.value)} type="radio" name='radio1' />{' '}
                             Edição com 8 páginas + 4 capas – Quantidade: 500 Exemplares - Valor: R$1.485,00
                         </Label>
                     </FormGroup>
                     <FormGroup style={{ paddingTop: '10px' }} check row>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Edição com 8 páginas + 4 capas – Quantidade: 1.000 Exemplares - Valor: R$2.350,00' 
+                                type="radio" onChange={e => setExemplar(e.target.value)} name='radio1' />{' '}
                             Edição com 8 páginas + 4 capas – Quantidade: 1.000 Exemplares - Valor: R$2.350,00
                         </Label>
                     </FormGroup>
                     <FormGroup style={{ paddingTop: '10px' }} check row>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Edição com 8 páginas + 4 capas – Quantidade: 2.000 Exemplares - Valor: R$3.560,00' 
+                                type="radio" onChange={e => setExemplar(e.target.value)} name='radio1' />{' '}
                             Edição com 8 páginas + 4 capas – Quantidade: 2.000 Exemplares - Valor: R$3.560,00
                         </Label>
                     </FormGroup>
                     <FormGroup style={{ paddingTop: '10px' }} check row>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Edição com 8 páginas + 4 capas – Quantidade: 4.000 Exemplares - Valor: R$7.120,00' 
+                                type="radio" onChange={e => setExemplar(e.target.value)}name='radio1' />{' '}
                             Edição com 8 páginas + 4 capas – Quantidade: 4.000 Exemplares - Valor: R$7.120,00
                         </Label>
                     </FormGroup>
                     <FormGroup style={{ paddingTop: '10px' }} check row>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Edição com 8 páginas + 4 capas – Quantidade: 5.000 Exemplares - Valor: R$8.900,00' 
+                                type="radio" onChange={e => setExemplar(e.target.value)}name='radio1' />{' '}
                             Edição com 8 páginas + 4 capas – Quantidade: 5.000 Exemplares - Valor: R$8.900,00
                         </Label>
                     </FormGroup>
                     <FormGroup style={{ paddingTop: '10px' }} check row>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Edição com 8 páginas + 4 capas – Quantidade: 8.000 Exemplares - Valor: R$17.400,00' 
+                                type="radio" onChange={e => setExemplar(e.target.value)}name='radio1' />{' '}
                             Edição com 8 páginas + 4 capas – Quantidade: 8.000 Exemplares - Valor: R$17.400,00
                         </Label>
                     </FormGroup>
                     <FormGroup style={{ paddingTop: '10px' }} check row>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Edição com 12 páginas + 4 capas – Quantidade: 250 Exemplares - Valor: R$1.087,50' 
+                                type="radio" onChange={e => setExemplar(e.target.value)} name='radio1' />{' '}
                             Edição com 12 páginas + 4 capas – Quantidade: 250 Exemplares - Valor: R$1.087,50
                         </Label>
                     </FormGroup>
                     <FormGroup style={{ paddingTop: '10px' }} check row>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Edição com 12 páginas + 4 capas – Quantidade: 500 Exemplares - Valor: R$1.905,00' 
+                                type="radio" onChange={e => setExemplar(e.target.value)} name='radio1' />{' '}
                             Edição com 12 páginas + 4 capas – Quantidade: 500 Exemplares - Valor: R$1.905,00
                         </Label>
                     </FormGroup>
                     <FormGroup style={{ paddingTop: '10px' }} check row>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Edição com 12 páginas + 4 capas – Quantidade: 1.000 Exemplares - Valor: R$3.050,00' 
+                                type="radio" onChange={e => setExemplar(e.target.value)}name='radio1' />{' '}
                             Edição com 12 páginas + 4 capas – Quantidade: 1.000 Exemplares - Valor: R$3.050,00
                         </Label>
                     </FormGroup>
                     <FormGroup style={{ paddingTop: '10px' }} check row>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Edição com 12 páginas + 4 capas – Quantidade: 2.000 Exemplares - Valor: R$4.680,00' 
+                                type="radio" onChange={e => setExemplar(e.target.value)} name='radio1' />{' '}
                             Edição com 12 páginas + 4 capas – Quantidade: 2.000 Exemplares - Valor: R$4.680,00
                         </Label>
                     </FormGroup>
                     <FormGroup style={{ paddingTop: '10px' }} check row>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Edição com 12 páginas + 4 capas – Quantidade: 4.000 Exemplares - Valor: R$9.360,00' 
+                                type="radio" onChange={e => setExemplar(e.target.value)} name='radio1' />{' '}
                             Edição com 12 páginas + 4 capas – Quantidade: 4.000 Exemplares - Valor: R$9.360,00
                         </Label>
                     </FormGroup>
                     <FormGroup style={{ paddingTop: '10px' }} check row>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Edição com 12 páginas + 4 capas – Quantidade: 5.000 Exemplares - Valor: R$11.700,00' 
+                                type="radio" onChange={e => setExemplar(e.target.value)} name='radio1' />{' '}
                             Edição com 12 páginas + 4 capas – Quantidade: 5.000 Exemplares - Valor: R$11.700,00
                         </Label>
                     </FormGroup>
                     <FormGroup style={{ paddingTop: '10px' }} check row>
                         <Label style={{ paddingRight: '10px' }}check>
-                            <Input type="radio" name='radio1' />{' '}
+                            <Input value='Edição com 12 páginas + 4 capas – Quantidade: 8.000 Exemplares - Valor: R$23.000,00' 
+                                type="radio" onChange={e => setExemplar(e.target.value)} name='radio1' />{' '}
                             Edição com 12 páginas + 4 capas – Quantidade: 8.000 Exemplares - Valor: R$23.000,00
                         </Label>
                     </FormGroup>
                 </div>
                 <FormGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="Nome do Representante Comercial:" />
+                        <Input onChange={e => setNomeDoRepresentanteComercial(e.target.value)} placeholder="Nome do Representante Comercial:" />
                     </InputGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <Input placeholder="Cidade do Representante Comercial" />
+                        <Input onChange={e => setCidadeDoRepresentanteComercial(e.target.value)} placeholder="Cidade do Representante Comercial" />
                     </InputGroup>
                     <InputGroup size="lg" style={{ paddingTop: '10px' }}>
-                        <CustomInput multiple type="file" id="exampleCustomFileBrowser" name="customFile" />
+                        <Input onChange={e => handleUpload(e.target.files)} multiple type="file" id="exampleCustomFileBrowser" name="file" />
                     </InputGroup>
                     <p style={{ textAlign: 'center', color: '#000264', fontFamily: 'Comic Sans MS', fontSize: '25px', padding: '15px' }}>
                         Solicite a confecção da arte final à gráfica de sua preferência ou a um arte-finalista para <br />
@@ -170,7 +236,7 @@ const Formulario1 = () => {
                     </p>
                 </FormGroup>
                 <div style={{ textAlign: 'center', paddingTop: '5px' }}>
-                    <Button outline color="primary">Enviar</Button>
+                    <Button onClick={() => enviarFormulario()} outline color="primary">Enviar</Button>
                 </div>
             </Form>
         </div>
