b2551355428c577aa299f7df91b036cf1aa47e69
viniciusll <viniciuslemos179@gmail.com>
2020-08-31 11:23:01 -0300

    alterações

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ import Candidatos from './views/Candidatos/candidatos';
 import Revistas from './views/Revistas/revistas.jsx';
 import CandidatosInscritos from './views/CandidatosInscritos/CandidatosInscritos';
 import RepresentantesCadastrados from './views/RepresentantesCadastrados/RepresentantesCadastrados'
+import Banner from './components/banner/banner';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
 
 var hist = createBrowserHistory();
 
@@ -27,6 +30,14 @@ ReactDOM.render(
             <Route path="/revistas" component={Revistas} />
             <Route path="/candidatos-inscritos" component={CandidatosInscritos} />
             <Route path="/representantes-cadastrados" component={RepresentantesCadastrados} />
+            <Route path="/revistas-propria" component={() => {
+                return <div>
+                        <Header />
+                        <Banner />
+                        <h2 style={{ textAlign: 'center', paddingTop: '20px', color: '#ff0000', fontFamily: 'Batang' }} >REVISTA PRÓPRIA</h2>
+                        <Footer />
+                    </div>
+            } } />
             <Route path="/" component={Components} />
         </Switch>
     </HashRouter>,
