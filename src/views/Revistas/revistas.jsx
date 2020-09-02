342d54691b8fb48dc64d87fa3830d338e9dd7014
viniciusll <viniciuslemos179@gmail.com>
2020-09-02 10:32:43 -0300

    revistas

--- src/views/Revistas/revistas.jsx
+++ src/views/Revistas/revistas.jsx
@@ -12,7 +12,6 @@ import ScrollToTopOnMount from '../../components/Scroll/scrollToTop';
 import { Document, Page, pdfjs } from 'react-pdf';
 import RevistaDemocraciaDoPovo1 from './1002 Revista Democracia do Povo 8 p. site.pdf';
 import RevistaDemocraciaDoPovo2 from './1011 Revist Panfleto Revista 12 Cand. site.pdf';
-import { Link } from 'react-router-dom';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -94,83 +93,14 @@ const Revistas = () => {
                     <p style={{ textAlign: 'center', color: '#19a9bf', fontFamily: 'Segoe Print', fontSize: '20px', padding: '15px' }}>
                         Revista Democracia do Povo. Edição com 8 páginas + as suas 4 capas.
                     </p>
-                    <div>
-                        <a href='https://drive.google.com/file/d/1vnhSJSNdxfyIQOPJ43ese3AZ3GtvW83C/view?usp=sharing'>imprimir Revista 1</a>
-                    </div>
-                    <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', backgroundColor: '#171616', flexWrap: 'wrap' }}>
-                        <button style={{
-                            backgroundColor: '#171616',
-                            color: '#fff',
-                            fontSize: '50px',
-                            border: 'none',
-                            outline: 'inherit'
-                        }}
-                            onClick={() => previousPage(pageNumber1)}
-                        >
-                            {'<'}
-                        </button>
-                        <Document
-                            file={RevistaDemocraciaDoPovo1}
-                            onLoadSuccess={onDocumentLoadSuccess1}
-                        >
-                            <Page style={{ width: '200%' }} pageNumber={pageNumber1} />
-                        </Document>
-                        <button style={{
-                            backgroundColor: '#171616',
-                            color: '#fff',
-                            fontSize: '50px',
-                            border: 'none',
-                            outline: 'inherit'
-                        }}
-                            onClick={() => nextPage(pageNumber1)}
-                        >
-                            {'>'}
-                        </button>
-                    </div>
-                    <p style={{ textAlign: 'center', color: '#8f1e3e', fontFamily: 'Apple Chancery, cursive', fontSize: '15px' }}>Page {pageNumber1} of {numPages1}</p>
+                    <iframe style={{ width: '100%', height: '80vh'}} src="https://drive.google.com/file/d/1vnhSJSNdxfyIQOPJ43ese3AZ3GtvW83C/preview"></iframe>
                     <p style={{ textAlign: 'center', color: '#19a9bf', fontFamily: 'Segoe Print', fontSize: '20px', padding: '15px' }}>
                         Revista Democracia do Povo. Edição com 12 páginas + as suas 4 capas.
                     </p>
                     <p style={{ textAlign: 'center', color: '#19a9bf', fontFamily: 'Segoe Print', fontSize: '20px', padding: '15px' }}>
                         Revista Panfleto em Revista. Edição com 4, 8 ou 12 candidatos do mesmo partido político.
                     </p>
-                    <div>
-                        <a href='https://drive.google.com/file/d/1dXgwRZyes_YQDC2I6vi0wmmMCXmIfpqr/view?usp=sharing'>imprimir Revista 2</a>
-                    </div>
-                    <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', backgroundColor: '#171616', flexWrap: 'wrap' }}>
-                        <button style={{
-                            backgroundColor: '#171616',
-                            color: '#fff',
-                            fontSize: '50px',
-                            border: 'none',
-                            outline: 'inherit'
-                        }}
-                            onClick={() => previousPage2(pageNumber2)}
-                        >
-                            {'<'}
-                        </button>
-                        <Document
-                            onLoadProgress={({ loaded, total }) => {
-                                return <Progress striped value={(loaded / total) * 100 + '%'} />
-                            }}
-                            file={RevistaDemocraciaDoPovo2}
-                            onLoadSuccess={onDocumentLoadSuccess2}
-                        >
-                            <Page style={{ width: '200%' }} pageNumber={pageNumber2} />
-                        </Document>
-                        <button style={{
-                            backgroundColor: '#171616',
-                            color: '#fff',
-                            fontSize: '50px',
-                            border: 'none',
-                            outline: 'inherit'
-                        }}
-                            onClick={() => nextPage2(pageNumber2)}
-                        >
-                            {'>'}
-                        </button>
-                    </div>
-                    <p style={{ textAlign: 'center', color: '#8f1e3e', fontFamily: 'Apple Chancery, cursive', fontSize: '15px' }}>Page {pageNumber2} of {numPages2}</p>
+                    <iframe style={{ width: '100%', height: '80vh'}} src="https://drive.google.com/file/d/1dXgwRZyes_YQDC2I6vi0wmmMCXmIfpqr/preview"></iframe>
                     <p style={{ textAlign: 'center', color: '#19a9bf', fontFamily: 'Segoe Print', fontSize: '20px', padding: '15px' }}>
                         Revista Democracia do Povo. As edições com 8 e 12 páginas são sínteses desta edição com 44 páginas.
                     </p>
