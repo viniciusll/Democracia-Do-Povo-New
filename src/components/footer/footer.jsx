e4973e28362e6cfbfc6e9f491e9b46b2352dea14
viniciusll <viniciuslemos179@gmail.com>
2020-09-04 10:17:32 -0300

    footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,8 +5,17 @@ import { Container, Row, Col } from 'reactstrap';
 const Footer = () => {
     return (
         <div className="footer4 b-t spacer" style={{  backgroundColor: '#003d03' }}>
-            <Container style={{alignItems: 'center'}}>
-                <p>Copyright © 2020 Todos os direitos reservados | Democracia Do Povo </p>
+            <Container>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <div>
+                        <p style={{ color: '#FFF' }}>SÓCIO BR PRODUTOS E SERVIÇOS INOVADORES EIRELI.</p>
+                        <p style={{ color: '#FFF' }}>Rua bom sucesso 407 Tatuapé - São Paulo - SP.</p>
+                        <p style={{ color: '#FFF' }}>CNPJ: 28.398.851/0001-53</p>
+                    </div>
+                    <div>
+                        <p style={{ color: '#FFF' }}>Copyright © 2020 Todos os direitos reservados | Democracia Do Povo </p>
+                    </div>
+                </div>
             </Container>
         </div>
     );
